b8a0674b179ffe3a9d644e377567a0c0c72ff4f6
Chandmanish-web <manish2003ban@gmail.com>
2026-07-31 18:09:26 +0530

    Latest Package

--- backend/seedTickets.js
+++ backend/seedTickets.js
@@ -235,10 +235,7 @@ async function seedTickets() {
     process.exit(1);
   }
 
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 
   console.log("Connected to MongoDB, seeding tickets...");
 
